915e5ca15e7d26b35a6ce44aba9f321e9f7dc0db
Gabrieli Barbosa <ifc.gabrieli.barbosa@gmail.com>
2019-06-18 13:29:26 -0300

    .

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,  ViewChild } from '@angular/core';
+import { ThfModalComponent, ThfNotificationService, ThfCheckboxGroupOption, ThfComboOption, ThfModalAction } from '@totvs/thf-ui';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -25,6 +27,73 @@ export class HomeComponent {
 
   redirectLink(link: string) {
     window.open(link, '_blank');
+  }
+
+  accompaniment: string = '';
+  fruits: Array<string>;
+  orderDetail: string = '';
+
+  close: ThfModalAction = {
+    action: () => {
+      this.closeModal();
+    },
+    label: 'Close',
+    danger: true
+  };
+
+  confirm: ThfModalAction = {
+    action: () => {
+      this.proccessOrder();
+    },
+    label: 'Confirm'
+  };
+
+  public readonly accompanimentOptions: Array<ThfComboOption> = [
+    {value: 'chocolate', label: 'Chocolate'},
+    {value: 'hazeinut', label: 'Hazelnut' },
+    {value: 'milk', label: 'Milk'}
+  ];
+
+  public readonly fruitsOptions: Array<ThfCheckboxGroupOption> = [
+    {value: 'orange', label: 'Orange'},
+    {value: 'apple', label: 'Apple' },
+    {value: 'pineapple', label: 'Pineapple'},
+    {value: 'graple', label: 'Grape' },
+    {value: 'strawberry', label: 'Strawberry'}
+  ];
+
+  @ViewChild('optionsForm') form: NgForm;
+  @ViewChild(ThfModalComponent) thfModal: ThfModalComponent;
+
+  constructor(private thfNotification: ThfNotificationService) { }
+
+  closeModal() {
+    this.form.reset();
+    this.thfModal.close();
+  }
+
+  openQuestionnaire() {
+    this.thfModal.open();
+  }
+
+  private proccessOrder() {
+
+    if (this.form.invalid) {
+      const orderInvalidMessage = 'Choose the items to confirm the order.';
+      this.thfNotification.warning(orderInvalidMessage);
+
+    } else {
+      this.confirm.loading = true;
+
+      setTimeout(() => {
+        this.thfNotification.success(`Your order confirmed: ${this.fruits}, with accompaniment: ${this.accompaniment}.`);
+        this.confirm.loading = false;
+        this.closeModal();
+
+      }, 700);
+
+    }
+
   }
 
 }
